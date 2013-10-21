80c0a669ffc348af765cf9295e3ef42d7be23f9a
danrequiem <DAN@iMac-de-Daniel.local>
2013-10-21 13:02:38 -0500

    Merge

---
+++ OpenGl/OpenGl/Vector3f.cpp
@@ -0,0 +1,9 @@
+//
+//  Vector3f.cpp
+//  OpenGl
+//
+//  Created by Dan on 21/10/13.
+//  Copyright (c) 2013 Dan. All rights reserved.
+//
+
+#include "Vector3f.h"
---
+++ OpenGl/OpenGl/Vector3f.h
@@ -0,0 +1,22 @@
+//
+//  Vector3f.h
+//  OpenGl
+//
+//  Created by Dan on 21/10/13.
+//  Copyright (c) 2013 Dan. All rights reserved.
+//
+
+#ifndef __OpenGl__Vector3f__
+#define __OpenGl__Vector3f__
+
+#include <iostream>
+
+class Vector3f{
+    
+public:
+    float x = 0;
+    float y = 0;
+    float z = 0;
+};
+
+#endif /* defined(__OpenGl__Vector3f__) */
